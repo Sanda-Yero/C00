b29562feb377ac6055be5d6630a41871fa69bf10
Sanda-Yero <syak2579@gmail.com>
2025-05-16 15:31:58 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #verifie un fichier
-#read -p " nom fichier : filename
 echo ":"
 read filename
 #verifie le nom du fichier
 if [ -f "$filename" ]; then
     echo " '$filename' existe."
 else
-    echo " '$filename' n'exixte pas."
+    echo " '$filename' n'existe pas."
 fi
