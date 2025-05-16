23d4df243d7d9b7a10bd4185c2c13b500eb7ca0f
Sanda-Yero <syak2579@gmail.com>
2025-05-16 15:58:32 +0100

    Delete check_file_exist.sh

--- check_file_exist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Demande le nom du fichier
-echo "Entrez le nom du fichier à vérifier :"
-read filename
-if [ -f $filename ]; then
-    echo "Le fichier '$filename' existe."
-else
-    echo "Le fichier '$filename' n'existe pas."
-fi
